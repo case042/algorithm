b11fb6ef8d21e03563c0534919790c9bab715cc9
case042 <smileboy0318@naver.com>
2022-03-03 16:47:42 +0900

    숫자의 개수

---
+++ 2577go.py
@@ -0,0 +1,18 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+result = str(a*b*c)
+
+arr = []
+numbers = list(map(str, [0,1,2,3,4,5,6,7,8,9]))
+
+for number in numbers:
+    cnt = 0
+    for value in result:
+        if number == value:
+            cnt = cnt + 1
+    arr.append(cnt)
+
+for data in arr:
+    print(data)
