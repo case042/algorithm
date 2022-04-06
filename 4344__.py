820c1b9882d2eae51097095edcbbfba4933cfe3c
case042 <smileboy0318@naver.com>
2022-04-06 15:09:49 +0900

    평균은 넘겠지

--- 4344__.py
+++ 4344__.py
@@ -3,11 +3,11 @@ n = int(input())
 for i in range(n):
     cnt = 0
     user_input = list(map(int, input().split()))
-    scores_cnt,scores = user_input[0],user_input[1:]
+    scores_cnt, scores = user_input[0], user_input[1:]
     avg = sum(scores)/scores_cnt
 
     for score in scores:
         if score > avg:
             cnt += 1
-    x = cnt/scores_cnt*100
+    x = cnt/scores_cnt * 100
     print("%0.3f"%x + "%")
